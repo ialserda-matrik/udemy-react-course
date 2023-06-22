7c22132bf3424f505a36b8ce39c7ca2224e2c5a2
Ivo Alserda <info@matrik.nl>
2023-06-23 00:26:25 +0200

    Apply changes

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -66,49 +66,59 @@ function Header() {
 }
 
 function Menu() {
+  const numPizzas = pizzaData.length;
+
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <div>
-        {pizzaData.map((pizza) => (
-          <Pizza name={pizza.name} />
-        ))}
-      </div>
-      {/* <Pizza
-        name="Pizza Spinaci"
-        ingredients="Tomato, Mozarella, spinach and Ricotta cheese"
-        photoName="pizzas/spinaci.jpg"
-        price="10"
-      />
 
-      <Pizza
-        name="Pizza Funghi"
-        ingredients="Tomato, mushrooms"
-        photoName="pizzas/funghi.jpg"
-        price="12"
-      /> */}
+      {numPizzas > 0 ? (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : null}
     </main>
   );
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()} - We are currently open.
+      {isOpen ? (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
     </footer>
   );
 }
 
 function Pizza(props) {
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} width="200" />
+    <li className="pizza">
+      <img
+        src={props.pizzaObj.photoName}
+        alt={props.pizzaObj.name}
+        width="200"
+      />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
