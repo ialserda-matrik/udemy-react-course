854f49a4fe886a567966e7f90972a76990e8928c
Ivo Alserda <info@matrik.nl>
2023-07-05 19:39:00 +0200

    Create generic StarRating component

--- 06-usepopcorn/src/index.js
+++ 06-usepopcorn/src/index.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./components/StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating
+        maxRating={10}
+        color="blue"
+        defaultRating={3}
+        onSetRating={setMovieRating}
+      />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      defaultRating={3}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      size={24}
+      color="#fcc419"
+      className="test"
+    />
+    <Test />
   </React.StrictMode>
 );
+
+function Star() {}
 
